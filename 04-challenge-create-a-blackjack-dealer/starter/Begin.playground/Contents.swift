02139cb4686bb28efe26ba62ef7e3d2afa401b73
Demian Odasso <demianodasso@Demians-MacBook-Pro.local>
2022-12-27 20:03:29 -0300

    Resolution for blackjack challenge

--- 04-challenge-create-a-blackjack-dealer/starter/Begin.playground/Contents.swift
+++ 04-challenge-create-a-blackjack-dealer/starter/Begin.playground/Contents.swift
@@ -19,11 +19,23 @@ example(of: "Create a Blackjack card dealer") {
     }
     
     // Add code to update dealtHand here
-    
+      if hand.points > 21 {
+          dealtHand.send(completion: .failure(.busted))
+      } else {
+          dealtHand.send(hand)
+      }
   }
   
   // Add subscription to dealtHand here
-  
+    dealtHand.sink (receiveCompletion: {
+        if case let .failure(error) = $0 {
+            print(error)
+        }
+    }, receiveValue: { hand in
+        print("Points \(hand.points)")
+        print(hand)
+    }).store(in: &subscriptions)
+
   
   deal(3)
 }
